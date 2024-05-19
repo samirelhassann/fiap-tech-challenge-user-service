6e1299aad0be9bc1cc695a41f3c79355ea42be53
Samir El Hassan <samirelhassann@gmail.com>
2024-05-19 18:37:16 -0300

    ci: configure ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@
 
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
