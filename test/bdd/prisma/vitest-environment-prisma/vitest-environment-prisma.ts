c1ef45646f3df93e3314b1a4bcc7b173ed97fbdc
Samir El Hassan <samirelhassann@gmail.com>
2024-05-19 19:34:39 -0300

    ci: configure ci/cd

--- test/bdd/prisma/vitest-environment-prisma/vitest-environment-prisma.ts
+++ test/bdd/prisma/vitest-environment-prisma/vitest-environment-prisma.ts
@@ -1,17 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { randomUUID } from "crypto";
-import dotenv from "dotenv";
 import { execSync } from "node:child_process";
 import { Environment } from "vitest";
 
+import { env } from "@/config/env";
 import { PrismaClient } from "@prisma/client";
-
-dotenv.config();
 
 const prisma = new PrismaClient();
 
 function generateDatabaseURL(schema: string) {
-  const url = new URL(process.env.DATABASE_URL!);
+  const url = new URL(env.DATABASE_URL);
 
   url.searchParams.set("schema", schema);
 
